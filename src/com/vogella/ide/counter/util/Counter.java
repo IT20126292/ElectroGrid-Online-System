a45eae6878bfcaab96abd700985fbee9488fa1b9
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-04-12 23:18:46 +0530

    Initial Commit

---
+++ src/com/vogella/ide/counter/util/Counter.java
@@ -0,0 +1,21 @@
+package com.vogella.ide.counter.util;
+
+public class Counter {
+
+	  public int count (int x){
+		    // TODO check that x > 0 and <= 255
+		    // if not throw a new RuntimeException
+		    // Example for a RuntimeException:
+
+
+		    // TODO calculate the numbers from 1 to x
+		    // for example if x is 5, calculate
+		    // 1 + 2 + 3 + 4 + 5
+
+
+		    // TODO return your calculated value
+		    // instead of 0
+		    return 0;
+
+	  }
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module com.vogella.eclipse.ide.first {
+}
