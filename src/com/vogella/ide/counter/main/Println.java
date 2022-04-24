5009aa6bc275e5bad078bd8397ef06761c255674
Kavisha Silva <it20126292@my.sliit.lk>
2022-04-25 02:35:46 +0530

    lastDay_Commit

--- src/com/vogella/ide/counter/main/Println.java
+++ src/com/vogella/ide/counter/main/Println.java
@@ -3,5 +3,8 @@ package com.vogella.ide.counter.main;
 public class Println{
     public static void main(String[] args){
         System.out.println("Helloooo");
+
+        System.out.println("Helloooo");
+
     }
 }
