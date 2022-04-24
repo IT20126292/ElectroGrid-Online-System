2c95a627ec670576c2eef7093cc40cd84006a078
Kavisha Silva <it20126292@my.sliit.lk>
2022-04-25 02:41:02 +0530

    todays-2002/04/25

--- src/com/vogella/eclipse/ide/first/MyFirstClass.java
+++ src/com/vogella/eclipse/ide/first/MyFirstClass.java
@@ -4,6 +4,16 @@ public class MyFirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Test Output");
+
+		System.out.println("Test Output");
+
+		int x = 1;
+
+		if(x>0){
+			System.out.println("Test Output");
+		}else{
+			System.out.println("Test Output");
+		}
 	}
 
 }
