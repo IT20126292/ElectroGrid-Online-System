16ffce862e52fb301e53b8a24aa0fcb9c9ea8a1a
Kavisha Silva <it20126292@my.sliit.lk>
2022-04-26 05:45:36 +0530

    todays-commit_2022/04/26

---
+++ src/com/vogella/ide/counter/main/function.java
@@ -0,0 +1,10 @@
+package com.vogella.ide.counter.main;
+
+public class Println{
+    public static void main(String[] args){
+        System.out.println("Helloooo");
+
+        System.out.println("function");
+
+    }
+}
